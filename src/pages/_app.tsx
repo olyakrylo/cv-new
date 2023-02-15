c73cefb29e15a25558b1bfca0b225be671762b66
Olga <olyakrylo24@gmail.com>
2023-02-16 01:41:15 +0400

    manual font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,10 @@
-import { Noto_Sans } from '@next/font/google';
 import type { AppProps } from 'next/app';
 
 import '@/shared/styles/globals.css';
 
-export const Typography = Noto_Sans({
-  weight: ['300', '400', '700', '800'],
-  subsets: ['latin', 'cyrillic'],
-});
-
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <main className={Typography.className}>
+    <main>
       <Component {...pageProps} />{' '}
     </main>
   );
